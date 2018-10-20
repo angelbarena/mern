e25007f37c11b2b3f1a14f312aec12114a1517d6
angel-barena <angelbarena89@hotmail.com>
2018-10-20 14:08:09 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.json({hi: 'there'});
+    res.json({hi: 'there dude'});
 });
 
 const PORT = process.env.PORT || 5000;
